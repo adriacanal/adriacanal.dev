668ce4fbbb41a772b0aca3b42f95212ab7e39666
Adrià Canal <adriacanal92@gmail.com>
2022-02-19 10:03:10 +0100

    Add web config file

---
+++ config/web.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'domain' => 'adriacanal.dev',
+    'description' => "My name is Adrià Canal and I'm Backend developer using Laravel.
+                      From Vilajuïga (Alt Empordà), Girona.
+                      I work at GNA Hotel Solutions since 2018",
+    'social' => [
+        'github' => 'https://github.com/adriacanal',
+        'twitter' => 'https://twitter.com/nokkerzz',
+        'linkedin' => 'https://www.linkedin.com/in/adria-canal'
+    ],
+    'contact' => [
+        'mail' => env('CONTACT_MAIL', 'hello@adriacanal.dev')
+    ]
+];
