39197db73e980bbd2b4e3d0d98375ebe2ed4cc1c
Adrià Canal <adriacanal92@gmail.com>
2022-02-21 23:18:39 +0100

    Fix contact submit

--- app/Http/Livewire/Contact.php
+++ app/Http/Livewire/Contact.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Contact extends Component
 {
+    public string $name = '';
+    public string $surname = '';
+    public string $email = '';
+    public string $message = '';
+
+    public function submit(): void
+    {
+        $this->validate([
+            'name'    => 'required',
+            'surname' => 'required',
+            'email'   => 'required|email',
+            'message' => 'required|min:10'
+        ]);
+
+        Mail::to(config('web.contact.mail'))->send(new ContactMail($this->name, $this->surname, $this->email, $this->message));
+
+        $this->name = '';
+        $this->surname = '';
+        $this->email = '';
+        $this->message = '';
+
+        session()->flash('title', 'Mail sent correctly 📬');
+        session()->flash('description', 'Thank you for contact me! I\'ll reply you as soon as possible 😄');
+    }
+
     public function render(): View
     {
         return view('livewire.contact');
---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailable;
+
+class ContactMail extends Mailable
+{
+    public function __construct(private string $name, private string $surname, private string $email, private string $message){}
+
+    public function build(): ContactMail
+    {
+        return $this->subject("📬 $this->name $this->surname is contacting through " . config('web.domain'))
+            ->markdown('emails.contact', [
+                'name'    => $this->name,
+                'surname' => $this->surname,
+                'email'   => $this->email,
+                'message' => $this->message
+            ]);
+    }
+}
