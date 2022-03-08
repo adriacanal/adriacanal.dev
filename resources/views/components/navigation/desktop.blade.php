5eaafa08013344b57e50f33efc206fef858dbb17
Adrià Canal <adriacanal92@gmail.com>
2022-03-08 11:35:48 +0100

    Disable uses page from menu

--- resources/views/components/navigation/desktop.blade.php
+++ resources/views/components/navigation/desktop.blade.php
@@ -3,9 +3,10 @@
     <a href="{{ route('page', 'about-me') }}" class="text-base font-medium text-gray-500 hover:text-green-800">
         About me
     </a>
-    <a href="{{ route('page', 'uses') }}" class="text-base font-medium text-gray-500 hover:text-green-800">
+    {{--TODO: This page is work in progress, disabled from menu for now --}}
+    {{--<a href="{{ route('page', 'uses') }}" class="text-base font-medium text-gray-500 hover:text-green-800">
         Uses
-    </a>
+    </a>--}}
     <a href="{{ route('contact') }}" class="text-base font-medium text-gray-500 hover:text-green-800">
         Contact
     </a>
