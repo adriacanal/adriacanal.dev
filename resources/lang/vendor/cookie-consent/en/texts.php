9d91113e27326acb5e964fb4953d2c3974e28176
Adrià Canal <adriacanal92@gmail.com>
2022-03-12 01:51:19 +0100

    Personalize cookie consent bannerr

---
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'I use my own and third-party cookies to analyze your browsing and collect statistical information. To accept the use of cookies you can select Allow cookies. For more information on the use of cookies and your rights, access the <a href="'. route('page', 'cookies') .'" title="Cookies Policy" class="underline">Cookies Policy</a>.',
+    'agree' => 'Allow cookies',
+];
--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,17 +1,15 @@
-<div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-2">
-    <div class="max-w-7xl mx-auto px-6">
-        <div class="p-2 rounded-lg bg-green-100">
-            <div class="flex items-center justify-between flex-wrap">
-                <div class="w-0 flex-1 items-center hidden md:inline">
-                    <p class="ml-3 text-green-800 cookie-consent__message">
-                        {!! trans('cookie-consent::texts.message') !!}
-                    </p>
-                </div>
-                <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
-                    <a class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-white bg-green-800 hover:bg-green-700">
-                        {{ trans('cookie-consent::texts.agree') }}
-                    </a>
-                </div>
+<div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0">
+    <div class="p-2 bg-green-100">
+        <div class="flex items-center justify-between flex-wrap">
+            <div class="flex-1 items-center md:inline">
+                <p class="ml-3 text-green-800 cookie-consent__message">
+                    {!! trans('cookie-consent::texts.message') !!}
+                </p>
+            </div>
+            <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
+                <a class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-white bg-green-800 hover:bg-green-700">
+                    {{ trans('cookie-consent::texts.agree') }}
+                </a>
             </div>
         </div>
     </div>
