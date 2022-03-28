964470dba835d98f30c962270501f2edf37b46f8
Adrià Canal <adriacanal92@gmail.com>
2022-03-28 21:44:30 +0200

    Update text on contact banner

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -179,7 +179,7 @@
     <div class="bg-gray-50 dark:bg-slate-800">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-                <span class="block">May I help you?</span>
+                <span class="block">Does your project needs some love?</span>
                 <span class="block text-green-800 dark:text-green-500">Get in touch with me!</span>
             </h2>
             <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
