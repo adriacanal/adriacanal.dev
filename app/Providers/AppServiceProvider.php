cd96516b0f8820e6cc4294536a8d0b633a5e2fba
Adrià Canal <adriacanal92@gmail.com>
2022-05-05 00:01:25 +0200

    Add secure cookie

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Config;
 use Illuminate\Support\ServiceProvider;
+use Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /*
+         * Set a default cookie prefix
+         * Spec https://tools.ietf.org/html/draft-ietf-httpbis-cookie-prefixes-00#section-3
+         */
+        if ((!preg_match('/^__/', Config::get('session.cookie'))) && (Config::get('session.secure') == true) && (Request::secure())) {
+            // Set the host prefix
+            if (strtolower(Config::get('session.cookie_prefix')) === 'host') {
+                // Set the host prefix
+                Config::set('session.cookie', '__Host-' . Config::get('session.cookie'));
+                Config::set('session.secure', true);
+                Config::set('session.path', '/');
+            } elseif (strtolower(Config::get('session.cookie_prefix')) !== 'none') {
+                // Set the secure prefix
+                Config::set('session.cookie', '__Secure-' . Config::get('session.cookie'));
+                Config::set('session.secure', true);
+            }
+        }
     }
 }
