d038365bf12a643ea4abc50a4b39c05f91dec144
Adrià Canal <adriacanal92@gmail.com>
2022-05-04 23:49:32 +0200

    Apply security headers

---
+++ app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+        //$response->headers->set('Content-Security-Policy', "default-src 'self'");
+        $response->headers->set('Referrer-Policy', 'same-origin');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('X-Frame-Options', 'deny');
+        $response->headers->set('Permissions-Policy', 'camera=(), display-capture=(), geolocation=(), microphone=()');
+        return $response;
+    }
+}
